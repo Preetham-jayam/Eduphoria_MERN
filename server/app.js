6f9abb30db838fd9024a548519aeac86e52d65a1
Preetham-jayam <preethamjayam2004@gmail.com>
2024-04-28 14:33:28 +0530

    cors update

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const accessLogStream = rfs.createStream('access.log', {
 app.use(morgan('combined', { stream: accessLogStream }));
 
 const corsOptions = {
-  origin: ['https://eduphoria-mern-frontend.vercel.app', 'http://localhost:3000'],
+  origin: ['https://frontend-mern-delta.vercel.app', 'http://localhost:3000'],
 };
 
 
